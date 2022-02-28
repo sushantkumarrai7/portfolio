8953c9707cf635ce71d785e87f3e720800a06651
sushantkumarrai7 <42889313+sushantkumarrai7@users.noreply.github.com>
2022-03-01 00:16:53 +0530

    1
    1

--- js/theme.js
+++ js/theme.js
@@ -200,6 +200,32 @@
 //    }
 //    testimonials_slider();
 	
+
+
+
+	/*----------------------------------------------------*/
+    /*  Back To Top Button
+    /*----------------------------------------------------*/
+
+
+var btn = $('#button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
+
+
+
+
 	
 	/*----------------------------------------------------*/
     /*  Google map js
